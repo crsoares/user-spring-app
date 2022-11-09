96744e198c71a69e8f2b2efc621074f6a21630a3
crsoares <crysthianophp@gmail.com>
2022-11-09 16:54:37 -0300

    classe user

---
+++ src/main/java/io/github/crsoares/api/domain/User.java
@@ -0,0 +1,21 @@
+package io.github.crsoares.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}

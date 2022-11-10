bdc012b5e0bf648cd485d657822fb80264712bea
crsoares <crysthianophp@gmail.com>
2022-11-10 10:51:51 -0300

    classe user dto

---
+++ src/main/java/io/github/crsoares/api/domain/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package io.github.crsoares.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Integer id;
+    private String nome;
+    private String email;
+    private String password;
+}

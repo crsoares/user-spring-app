17aa009946cfd444d8e4d4c987701289afc103d2
crsoares <crysthianophp@gmail.com>
2022-11-11 22:21:20 -0300

    endpoint create para user

--- src/main/java/io/github/crsoares/api/services/UserService.java
+++ src/main/java/io/github/crsoares/api/services/UserService.java
@@ -1,6 +1,7 @@
 package io.github.crsoares.api.services;
 
 import io.github.crsoares.api.domain.User;
+import io.github.crsoares.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }

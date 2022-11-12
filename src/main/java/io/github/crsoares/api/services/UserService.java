f9afe394e038881e7fdfdb2acec1fb229d879a0b
crsoares <crysthianophp@gmail.com>
2022-11-11 21:37:00 -0300

    endpoint findAll para user

--- src/main/java/io/github/crsoares/api/services/UserService.java
+++ src/main/java/io/github/crsoares/api/services/UserService.java
@@ -2,7 +2,10 @@ package io.github.crsoares.api.services;
 
 import io.github.crsoares.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }

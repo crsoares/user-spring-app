82002295088db6c554128aeb448808940acd83f8
crsoares <crysthianophp@gmail.com>
2022-11-24 12:14:12 -0300

    removendo anotacoes nao utilizadas

--- src/test/java/io/github/crsoares/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/io/github/crsoares/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package io.github.crsoares.api.resources.exceptions;
 
+import io.github.crsoares.api.services.exceptions.DataIntegratyViolationException;
 import io.github.crsoares.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,6 +42,20 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandarError> response = exceptionHandler.dataIntegrityViolationException(
+                new DataIntegratyViolationException("E-mail já cadastrado"),
+                new MockHttpServletRequest()
+        );
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandarError.class, response.getBody().getClass());
+        assertEquals("E-mail já cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 }
